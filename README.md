855aa27febcda8f4e7720fa82b93accdec107aa7
Yuxuan Xie <teststaybaka@gmail.com>
2021-11-13 00:14:09 +0000

    Migrate API definitions. Release

---
+++ README.md
@@ -0,0 +1,9 @@
+# @selfage/puppeteer_test_executor_api
+
+## Install
+
+`npm install @selfage/puppeteer_test_executor_api`
+
+## Overview
+
+Written in TypeScript and compiled to ES6 with inline source map & source. See [@selfage/tsconfig](https://www.npmjs.com/package/@selfage/tsconfig) for full compiler options. Provides API definitions for JS files running in browser context, executed by `@selfage/puppeteer_test_executor`. See [@selfage/puppeteer_test_executor](https://www.npmjs.com/package/@selfage/puppeteer_test_executor) for full document.
---
+++ apis.ts
@@ -0,0 +1,18 @@
+declare var argv: Array<string>;
+declare function exit(): void;
+declare function screenshot(
+  relativePath: string,
+  options: { delay?: number; fullPage?: boolean; quality?: number }
+): Promise<void>;
+declare function fileExists(relativePath: string): Promise<boolean>;
+declare function readFile(
+  relativePath: string,
+  // Not use BufferEncoding to avoid dependency on @types/node
+  encoding?: string
+): Promise<string | ArrayBuffer>;
+declare function writeFile(
+  relativePath: string,
+  data: ArrayBuffer
+): Promise<void>;
+declare function deleteFile(relativePath: string): Promise<void>;
+declare function setViewport(width: number, height: number): Promise<void>;
