a461ff8490b0a3f3e2916b6cf2d2048b2f8eb02b
Yuxuan Xie <teststaybaka@gmail.com>
2022-09-02 19:54:57 +0000

    Update new API names. Release

--- apis.ts
+++ apis.ts
@@ -1,21 +1,21 @@
-declare var argv: Array<string>;
-declare function exit(): void;
-declare function screenshot(
+declare var puppeteerArgv: Array<string>;
+declare function puppeteerExit(): void;
+declare function puppeteerScreenshot(
   relativePath: string,
   options?: { delay?: number; fullPage?: boolean; quality?: number }
 ): Promise<void>;
-declare function fileExists(relativePath: string): Promise<boolean>;
-declare function readFile(
+declare function puppeteerFileExists(relativePath: string): Promise<boolean>;
+declare function puppeteerReadFile(
   relativePath: string,
   // An encoding is mandatory because raw buffer data cannot be transmitted
   // correctly. Refer to BufferEncoding for all supported strings.
   encoding: string
 ): Promise<string>;
-declare function writeFile(
+declare function puppeteerWriteFile(
   relativePath: string,
   // Represented as a binary string.
   data: string
 ): Promise<void>;
-declare function deleteFile(relativePath: string): Promise<void>;
-declare function setViewport(width: number, height: number): Promise<void>;
-declare function mockExactFile(originalUrl: string, relativePath: string): void;
+declare function puppeteerDeleteFile(relativePath: string): Promise<void>;
+declare function puppeteerSetViewport(width: number, height: number): Promise<void>;
+declare function puppeteerMockExactFile(originalUrl: string, relativePath: string): void;
