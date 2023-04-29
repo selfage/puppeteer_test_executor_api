7d73515b817ab054bab187ef26aeb29a8c62d729
Yuxuan Xie <teststaybaka@gmail.com>
2023-04-29 07:22:29 +0000

    Move APIs back to its own repo. Release

--- apis.ts
+++ apis.ts
@@ -1,33 +1,69 @@
-declare var puppeteerArgv: Array<string>;
-declare function puppeteerExit(): void;
-declare function puppeteerScreenshot(
+import "./global_apis";
+
+export function getArgv(): Array<string> {
+  return globalThis.puppeteerArgv;
+}
+
+export function exit(): void {
+  return globalThis.puppeteerExit();
+}
+
+export function screenshot(
   relativePath: string,
   options?: { delay?: number /* ms */; fullPage?: boolean; quality?: number }
-): Promise<void>;
-declare function puppeteerFileExists(relativePath: string): Promise<boolean>;
-declare function puppeteerReadFile(
+): Promise<void> {
+  return globalThis.puppeteerScreenshot(relativePath, options);
+}
+
+export function fileExists(relativePath: string): Promise<boolean> {
+  return globalThis.puppeteerFileExists(relativePath);
+}
+
+export function readFile(
   relativePath: string,
   // An encoding is mandatory because raw buffer data cannot be transmitted
   // correctly. Refer to BufferEncoding for all supported strings.
   encoding: string
-): Promise<string>;
-declare function puppeteerWriteFile(
+): Promise<string> {
+  return globalThis.puppeteerReadFile(relativePath, encoding);
+}
+
+export function writeFile(
   relativePath: string,
   // Represented as a binary string.
   data: string
-): Promise<void>;
-declare function puppeteerDeleteFile(relativePath: string): Promise<void>;
-declare function puppeteerSetViewport(
-  width: number,
-  height: number
-): Promise<void>;
-declare function puppeteerMockExactFile(
-  originalUrl: string,
-  relativePath: string
-): void;
-declare function puppeteerWaitForFileChooser(
-  delayAfter?: number /* ms */
-): Promise<void>;
-declare function puppeteerFileChooserAccept(
+): Promise<void> {
+  return globalThis.puppeteerWriteFile(relativePath, data);
+}
+
+export function deleteFile(relativePath: string): Promise<void> {
+  return globalThis.puppeteerDeleteFile(relativePath);
+}
+
+export async function getFiles(
   ...relativePaths: Array<string>
-): Promise<void>;
+): Promise<FileList> {
+  let fileInput = document.createElement("input");
+  fileInput.type = "file";
+  await globalThis.puppeteerWaitForFileChooser();
+  fileInput.click();
+  await globalThis.puppeteerFileChooserAccept(...relativePaths);
+  return fileInput.files;
+}
+
+export async function supplyFiles(
+  triggerFileChooserFn: () => void,
+  ...relativePaths: Array<string>
+): Promise<void> {
+  await globalThis.puppeteerWaitForFileChooser();
+  triggerFileChooserFn();
+  return globalThis.puppeteerFileChooserAccept(...relativePaths);
+}
+
+export function setViewport(width: number, height: number): Promise<void> {
+  return globalThis.puppeteerSetViewport(width, height);
+}
+
+export function mockExactFile(originalUrl: string, relativePath: string): void {
+  return globalThis.puppeteerMockExactFile(originalUrl, relativePath);
+}
---
+++ global_apis.ts
@@ -0,0 +1,33 @@
+declare var puppeteerArgv: Array<string>;
+declare function puppeteerExit(): void;
+declare function puppeteerScreenshot(
+  relativePath: string,
+  options?: { delay?: number /* ms */; fullPage?: boolean; quality?: number }
+): Promise<void>;
+declare function puppeteerFileExists(relativePath: string): Promise<boolean>;
+declare function puppeteerReadFile(
+  relativePath: string,
+  // An encoding is mandatory because raw buffer data cannot be transmitted
+  // correctly. Refer to BufferEncoding for all supported strings.
+  encoding: string
+): Promise<string>;
+declare function puppeteerWriteFile(
+  relativePath: string,
+  // Represented as a binary string.
+  data: string
+): Promise<void>;
+declare function puppeteerDeleteFile(relativePath: string): Promise<void>;
+declare function puppeteerSetViewport(
+  width: number,
+  height: number
+): Promise<void>;
+declare function puppeteerMockExactFile(
+  originalUrl: string,
+  relativePath: string
+): void;
+declare function puppeteerWaitForFileChooser(
+  delayAfter?: number /* ms */
+): Promise<void>;
+declare function puppeteerFileChooserAccept(
+  ...relativePaths: Array<string>
+): Promise<void>;
